265438bd92b272bb3bc0068993635cc9afda517d
jjosephd <jjosephd804@gmail.com>
2023-04-20 07:40:14 -0400

    added mock backend using json server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,21 +1,31 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'Sample Feedback Item',
-      rating: 10,
-    },
-  ]);
-
+  const [loading, setLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
   const [feedbackEdit, setFeedbackEdit] = useState({
     edit: false,
     item: {},
   });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  //Fetch Feedback data
+
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      `http://localhost:5000/feedback?_sort=id&_order=desc`
+    );
+
+    const data = await response.json();
+    setFeedback(data);
+    setLoading(false);
+  };
 
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete')) {
@@ -45,6 +55,7 @@ export const FeedbackProvider = ({ children }) => {
     <FeedbackContext.Provider
       value={{
         feedback,
+        loading,
         deleteFeedback,
         addFeedback,
         editFeedback,
